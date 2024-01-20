ea93d83ae187f984a337c7487123ff3de3de376a
Rodrigo <rodrigolucianocosta@gmail.com>
2024-01-20 13:41:20 -0300

    teste de blockchain

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ const bloco1 = new Block()
 
 const blockchain = new Blockchain()
 blockchain.addBlock([{from: 'a',to: 'b',amount: 10 }])
-blockchain.addBlock([{from: 'b',to: 'c',amount: 15}])
+blockchain.addBlock([{from: 'a',to: 'c',amount: 15}])
 // console.log(blockchain)
 // console.log(blockchain.isValid())
 
-blockchain.blocks[1].data = {from:'a',to: 'b',amount:1000}
-console.log(blockchain)
+// blockchain.blocks[1].data = {from:'a',to: 'b',amount:1000}
+// console.log(blockchain)
+console.log(JSON.stringify(blockchain))
+console.log(blockchain.isValid())
+
+blockchain.blocks[1].data = { from: 'a', to: 'b', amount: 2}
+console.log(JSON.stringify(blockchain))
 console.log(blockchain.isValid())
