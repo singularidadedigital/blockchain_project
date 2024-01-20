e1235b2e6b14481662550fc7e92eab9e916246a6
Rodrigo <rodrigolucianocosta@gmail.com>
2024-01-20 12:47:46 -0300

    alterando dados e validando hash

--- Blockchain.js
+++ Blockchain.js
@@ -15,4 +15,18 @@ module.exports = class Blockchain{
         const block = new Block(this.nextIndex++, hash, data)
         this.blocks.push(block)
     }
+
+    //verificando se a o hash é valido
+    isValid(){
+        for(let i = this.blocks.length-1;i>0;i--){
+            const currentBlock = this.blocks[i]
+            const previousBlock = this.blocks[i-1]
+            if(currentBlock.hash !== currentBlock.generateHash()
+            || currentBlock.previousHash !== previousBlock.hash
+            || currentBlock.index !== previousBlock.index + 1){
+                return false
+            }
+        }
+        return true
+    }
 }
--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const bloco1 = new Block()
 // console.log(bloco1)
 
 const blockchain = new Blockchain()
-blockchain.addBlock([{
-    from: 'a',
-    to: 'b',
-    amount: 10    
-}])
+blockchain.addBlock([{from: 'a',to: 'b',amount: 10 }])
+blockchain.addBlock([{from: 'b',to: 'c',amount: 15}])
+// console.log(blockchain)
+// console.log(blockchain.isValid())
 
+blockchain.blocks[1].data = {from:'a',to: 'b',amount:1000}
 console.log(blockchain)
+console.log(blockchain.isValid())
