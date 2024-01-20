b65abdff7e27c88df921236517f8c3e0bea88e07
Rodrigo <rodrigolucianocosta@gmail.com>
2024-01-20 10:55:48 -0300

    create a blockchain blocks

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const Block = require("./Block")
+const Blockchain = require("./Blockchain")
 
 const bloco1 = new Block()
-console.log(bloco1)
+// console.log(bloco1)
+
+const blockchain = new Blockchain()
+blockchain.addBlock([{
+    from: 'a',
+    to: 'b',
+    amount: 10    
+}])
+
+console.log(blockchain)
